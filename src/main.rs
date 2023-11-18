8f3c93f7840dbfbc4ad6f761199f95da99411cd6
Benedikt Brunner <122370755+Benedikt-Brunner@users.noreply.github.com>
2023-11-18 15:17:55 +0100

    Update project dependencies and add HTML file for htmx functionality

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,38 @@
-fn main() {
-    println!("Hello, world!");
+use axum::{
+    http::StatusCode,
+    response::{Html, IntoResponse, Response},
+    routing::{get, post},
+    Router,
+};
+use std::sync::atomic::{AtomicU8, Ordering};
+
+static COUNT: AtomicU8 = AtomicU8::new(0);
+
+#[tokio::main]
+async fn main() {
+    // build our application with a single route
+    let app = Router::new()
+        .route("/", get(hello))
+        .route("/clicked", post(click));
+
+    // run it with hyper on localhost:3000
+    axum::Server::bind(&"0.0.0.0:3000".parse().unwrap())
+        .serve(app.into_make_service())
+        .await
+        .unwrap();
+
+    async fn hello() -> Response {
+        match std::fs::read_to_string("./html/hello.html") {
+            Ok(content) => Html(content).into_response(),
+            Err(err) => (
+                StatusCode::INTERNAL_SERVER_ERROR,
+                format!("Failed to read file. Error: {}", err),
+            ).into_response(),
+        }
+    }
+
+    async fn click() -> Response {
+        COUNT.fetch_add(1, Ordering::Relaxed);
+        format!("Count: {}", COUNT.load(Ordering::Relaxed)).into_response()
+    }
 }
